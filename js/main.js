1b3074a6e7c761c2e73484c582570b03871a8af5
Abelardo <37087157+avillalobosd@users.noreply.github.com>
2021-10-14 15:27:46 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,175 @@
+
+
+; (function () {
+
+	'use strict';
+
+
+
+	var isMobile = {
+		Android: function () {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function () {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function () {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function () {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function () {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function () {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	var fullHeight = function () {
+
+		if (!isMobile.any()) {
+			$('.js-fullheight').css('height', $(window).height());
+			$(window).resize(function () {
+				$('.js-fullheight').css('height', $(window).height());
+			});
+		}
+
+	};
+
+	// Animations
+
+	var contentWayPoint = function () {
+		var i = 0;
+		$('.animate-box').waypoint(function (direction) {
+
+			if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function () {
+
+					$('body .animate-box.item-animate').each(function (k) {
+						var el = $(this);
+						setTimeout(function () {
+							var effect = el.data('animate-effect');
+							if (effect === 'fadeIn') {
+								el.addClass('fadeIn animated');
+							} else if (effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft animated');
+							} else if (effect === 'fadeInRight') {
+								el.addClass('fadeInRight animated');
+							} else {
+								el.addClass('fadeInUp animated');
+							}
+
+							el.removeClass('item-animate');
+						}, k * 200, 'easeInOutExpo');
+					});
+
+				}, 100);
+
+			}
+
+		}, { offset: '85%' });
+	};
+
+
+	var burgerMenu = function () {
+
+		$('.js-fh5co-nav-toggle').on('click', function (event) {
+			event.preventDefault();
+			var $this = $(this);
+
+			if ($('body').hasClass('offcanvas')) {
+				$this.removeClass('active');
+				$('body').removeClass('offcanvas');
+			} else {
+				$this.addClass('active');
+				$('body').addClass('offcanvas');
+			}
+		});
+
+
+
+	};
+
+	// Click outside of offcanvass
+	var mobileMenuOutsideClick = function () {
+
+		$(document).click(function (e) {
+			var container = $("#fh5co-aside, .js-fh5co-nav-toggle");
+			if (!container.is(e.target) && container.has(e.target).length === 0) {
+
+				if ($('body').hasClass('offcanvas')) {
+
+					$('body').removeClass('offcanvas');
+					$('.js-fh5co-nav-toggle').removeClass('active');
+
+				}
+
+			}
+		});
+
+		$(window).scroll(function () {
+			if ($('body').hasClass('offcanvas')) {
+
+				$('body').removeClass('offcanvas');
+				$('.js-fh5co-nav-toggle').removeClass('active');
+
+			}
+		});
+
+	};
+
+	var sliderMain = function () {
+
+		$('#fh5co-hero .flexslider').flexslider({
+			animation: "fade",
+			slideshowSpeed: 5000,
+			directionNav: true,
+			start: function () {
+				setTimeout(function () {
+					$('.slider-text').removeClass('animated fadeInUp');
+					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+				}, 500);
+			},
+			before: function () {
+				setTimeout(function () {
+					$('.slider-text').removeClass('animated fadeInUp');
+					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+				}, 500);
+			}
+
+		});
+
+	};
+
+
+
+	var addTemplates = function () {
+		var templates = ['burger', 'osteriax', 'profile', 'selecao', 'smbarber', 'touche', 'asymmetry', 'digitalagency']
+
+		templates.map(function (x) {
+			var i = '<div data-toggle="modal" data-target="#'+x+'" class="col-md-3 col-sm-6 col-padding text-center animate-box"><a href="#" class="work image-popup" style="background-image: url(templates/' + x + '/' + x + '.jpg);"><div class="desc"><h3>' + x + '</h3><span>Template</span></div></a></div>'
+			var modal = '<div class="modal fade" id="'+x+'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Modal title</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><img style="width:100%"src="templates/'+x+'/'+x+'_screen.png"></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button></div></div></div></div>'
+			$(".fotos").append(i);
+			$(".fotos").append(modal);
+		});
+
+	};
+
+	// Document on load.
+	$(function () {
+		addTemplates();
+		fullHeight();
+		contentWayPoint();
+		burgerMenu();
+		mobileMenuOutsideClick();
+		sliderMain();
+	});
+
+
+}());
